18f53dc65fee50bfb4625c71228674407c6aab22
Andreas Klein <mail@andreasklein.org>
2014-10-02 18:38:27 +0200

    Refactoring
    * Update npm dependencies to latest version
    * Add JavaScript Code Style Checker (JSCS)
    * Refactor Code Style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,117 +1,122 @@
-/*!
- * Gruntfile.js
- * @author Andreas Klein (ixisio)
+/**
+ * Gruntfile
+ *
+ * @author Andreas Klein
+ * @date 2013-05-26
  */
-
-
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   'use strict';
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   grunt.initConfig({
     globals: {
-      DEV_SERVER_PORT: 3322
+        DEV_SERVER_PORT: 3322
     },
     pkg: grunt.file.readJSON('package.json'),
-
-    // Project Configuration
-    // --------------------------
     jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
+        options: {
+            curly: true,
+            eqeqeq: true,
+            eqnull: true,
+            browser: true,
+            globals: {
+                jQuery: true
+            }
+        },
+        src: ['*.js', 'www/scripts/*.js']
+    },
+    jscs: {
+        src: '<%= jshint.src %>',
+        options: {
+            config: '.jscsrc'
         }
-      },
-      gruntfile: ['Gruntfile.js'],
-      all: ['Gruntfile.js', 'www/*.js', 'index.js'],
-      test: ['test/**/*.js']
     },
     less: {
-      main: {
-        options: {
-          compress: false,
-          report: false, // 'min'
-          dumpLineNumbers: 'comments'
-        },
-        files: {
-          'www/styles/main.css': 'www/styles/less/main.less',
-          'www/styles/views/desktop.css': 'www/styles/less/views/desktop.less'
+        main: {
+            options: {
+                compress: false,
+                report: false,
+                dumpLineNumbers: 'comments'
+            },
+            files: {
+                'www/styles/main.css': 'www/styles/less/main.less',
+                'www/styles/views/desktop.css': 'www/styles/less/views/desktop.less'
+            }
         }
-      }
     },
     autoprefixer: {
         options: {
-          browsers: ['last 1 version'],
-          report: 'min'
+            browsers: ['last 1 version'],
+            report: 'min'
         },
         css: {
-          src: ['www/styles/**/*.css']
+            src: ['www/styles/**/*.css']
         }
     },
     watch: {
-      less: {
-        files: ['www/styles/**/*.less'],
-        tasks: ['less', 'autoprefixer']
-      }
+        less: {
+            files: ['www/styles/**/*.less'],
+            tasks: ['less', 'autoprefixer']
+        },
+        js: {
+            files: ['*.js', 'www/scripts/*.js'],
+            tasks: ['jshint', 'jscs']
+        }
     },
     open: {
-      server: {
-        path: 'http://localhost:<%= globals.DEV_SERVER_PORT %>'
-      }
+        server: {
+            path: 'http://localhost:<%= globals.DEV_SERVER_PORT %>'
+        }
     },
-
-    // Prod-Build Config
-    // --------------------------
     cssmin: {
-      options: {
-        // banner: '/*! Stylsheets <%= pkg.name %> - v<%= pkg.version %> - ' +
-        //   '<%= grunt.template.today("yyyy-mm-dd") %> - <%= pkg.author.url %> */',
-        report: 'min'
-      },
-      dist: {
-        expand: true,
-        cwd: 'www/styles/',
-        src: ['**/*.css'],
-        dest: 'www/styles/'
-      }
+        options: {
+            report: 'min'
+        },
+        dist: {
+            expand: true,
+            cwd: 'www/styles/',
+            src: ['**/*.css'],
+            dest: 'www/styles/'
+        }
     }
   });
 
-
-  // Register Tasks
-  // (1) 'prod': Build for Prod enviroment
-  // (2) 'assets' : Lint & build static assets like (SASS Files)
-  // (3) 'dev' Development Workflow
-  // (4) default (run `dev` task)
-  // --------------------------
+  /**
+   * Register Tasks:
+   *
+   *   (1) 'prod': Build for Prod enviroment
+   *   (2) 'assets' : Lint & build static assets like (SASS Files)
+   *   (3) 'dev' Development Workflow
+   *   (4) default (run `dev` task)
+   */
   grunt.registerTask('prod', [
-    'jshint:all',
-    'less',
-    'autoprefixer',
-    'cssmin'
+      'jshint',
+      'jscs',
+      'less',
+      'autoprefixer',
+      'cssmin'
   ]);
+
   grunt.registerTask('assets', [
-    'jshint:all',
-    'less',
-    'autoprefixer'
+      'jshint',
+      'jscs',
+      'autoprefixer'
   ]);
-  grunt.registerTask('node-server', function() {
-    // Run node as child process
-    grunt.util.spawn({
-      cmd: 'node',
-      args: ['index.js']
-    });
+
+  grunt.registerTask('node-server', function () {
+      grunt.util.spawn({
+          cmd: 'node',
+          args: ['index.js']
+      });
   });
-  grunt.registerTask('dev', function() {
-    grunt.task.run('node-server');
-    grunt.task.run('assets');
-    grunt.task.run('open');
-    grunt.task.run('watch');
+
+  grunt.registerTask('dev', function () {
+      grunt.task.run('node-server');
+      grunt.task.run('assets');
+      grunt.task.run('open');
+      grunt.task.run('watch');
   });
+
   grunt.registerTask('default', ['dev']);
 };
